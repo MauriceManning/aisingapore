f1a544bd833e3c1034538f918db1f450b53dffe7
Maurice Manning <maurice.manning@gmail.com>
2018-06-06 14:12:29 +0800

    Update installscript1.sh

--- azurespark/installscript1.sh
+++ azurespark/installscript1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-/usr/bin/anaconda/bin/pip install azure-storage -n py35
-/usr/bin/anaconda/bin/pip install azure -n py35
+/usr/bin/anaconda/bin/conda install -c conda-forge azure-storage -n py35
+/usr/bin/anaconda/bin/conda install -c conda-forge azure -n py35
